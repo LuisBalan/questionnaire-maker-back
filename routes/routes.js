42d3b549432ab58eacc830a23966eabfcf7b612c
LuisBalan <jlbalcz@gmail.com>
2022-06-10 17:14:11 -0500

    fix get all questionnaries endpoint

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,33 @@
+const { response } = require('express');
 const express = require('express');
 const router = express.Router();
+const connection = require('../services/db');
 
 router.get("/", (req, res) => {
     res.json({mensaje: "Hola cuestionario!"});
 });
+
+
+router.get("/cuestionarios", (req, res) => {
+    connection.query("SELECT * FROM Cuestionarios", 
+    (error, results) => {
+        if(error){
+            throw error;
+        }else{
+            res.status(200).json(results);
+        }
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
